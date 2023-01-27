5ce083041a9a4b63c49d5eeaef1dc497cccf0baa
santi <santi@DESKTOP-N2CVM35>
2023-01-27 19:40:54 +0100

    este es mi primer control de version

--- src/ejerciciosedgit/EjerciciosEDgit.java
+++ src/ejerciciosedgit/EjerciciosEDgit.java
@@ -14,7 +14,7 @@ public class EjerciciosEDgit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        System.out.println("adfqfwef");
     }
-    
+   
 }
